3b0bf91d803591da89c4d772bc351c3b19e6ce25
juandesousa <juan.cdsb@gmail.com>
2021-08-13 09:38:51 +0200

    Homework

--- week-3/Homework/C-alarmclock/alarmclock.js
+++ week-3/Homework/C-alarmclock/alarmclock.js
@@ -1,4 +1,33 @@
-function setAlarm() {}
+let intervalID;
+
+function setAlarm() {
+  let alarmSet = document.getElementById('alarmSet').value;
+  const timeRemaining = document.getElementById('timeRemaining');
+  const body = document.querySelector('body');
+
+  clearInterval(intervalID)
+  body.style.background = 'white';
+  pauseAlarm();
+
+  intervalID = setInterval(()=>{
+  
+    if(alarmSet===0){
+      timeRemaining.innerText = `Time Remaining: 00:0${alarmSet}`;
+      body.style.background = 'tomato';
+      playAlarm();
+      clearInterval(intervalID)
+    }else if(alarmSet>9){
+      alarmSet = 9;
+      timeRemaining.innerText = `Time Remaining: 00:0${alarmSet}`;
+      alarmSet --
+    }else{
+      timeRemaining.innerText = `Time Remaining: 00:0${alarmSet}`;
+      alarmSet --
+    }
+
+  },1000)
+
+}
 
 // DO NOT EDIT BELOW HERE
 
